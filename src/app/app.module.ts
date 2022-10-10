2adb67a35bc64922f19f2fd6a89b19455c20d28d
Ansen123 <ansenfp@gmail.com>
2022-10-10 23:39:10 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewslistComponent } from './newslist/newslist.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes:Routes=[
+  {
+    path:"",component:NewslistComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +19,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
