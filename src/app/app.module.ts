8d7a417134409d31921765d79e6699ba047666c9
Ansen123 <ansenfp@gmail.com>
2022-10-10 23:23:05 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewslistComponent } from './newslist/newslist.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewslistComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
